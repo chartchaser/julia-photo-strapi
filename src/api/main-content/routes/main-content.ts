3e7f92862eb6957e36f36d2f8329e576592e9b94
Scott Petrarca <chartchaser@outlook.com>
2024-09-02 15:43:42 -0400

    initial commit

---
+++ src/api/main-content/routes/main-content.ts
@@ -0,0 +1,7 @@
+/**
+ * main-content router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::main-content.main-content');
---
+++ src/api/main-content/services/main-content.ts
@@ -0,0 +1,7 @@
+/**
+ * main-content service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::main-content.main-content');
